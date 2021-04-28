8457693b1435524cda2341b32a51f16a4ba83c6b
Dell-Komputer\Dell <oskarkusiak@gmail.com>
2021-04-28 20:58:04 +0200

    Dodano wywoływanie funkcji za pomocą buttonów
    Poprawki w:
    okienkaDialogowe.html
    okienkaDialogowe.js
    petle.html
    style.css

--- js/okienkaDialogowe.js
+++ js/okienkaDialogowe.js
@@ -3,7 +3,7 @@
 
 // alert(' zostaje wyświetlone okienko z komunikatem i przyciskiem OK. Po kliknięciu przycisku nie zostaje zwrócona żadna wartość, tak więc okno to nadaje się tylko do informowania użytkownika o pewnych zdarzeniach. Zaznaczyć trzeba, że nie ma możliwości zmiany tytułu tego okna (jest zależny od przeglądarki), a tylko treści jakie to okienko wyświetla.');
 
-function examlpe1(){
+function example1(){
     return alert('Przykładowy alert');
 }
 
@@ -20,16 +20,13 @@ function example2(){
 
 // Do funkcji prompt przekazujemy dwa parametry - jeden jest treścią, która będzie wyświetlana w okienku, a drugi jest domyślną wartością, która będzie wyświetlana w polu, w które wpisujemy tekst. Jeżeli użytkownik kliknie [ANULUJ], zostanie zwrócona wartość null, w przeciwnym razie zostanie zwrócona wartość wpisana w pole okienka.
 
-function example3{
+function example3(){
     const imie = prompt('Podaj swoje Imię', '');
     if(imie){
-        alert(`Masz na imię $imie`);
+        alert(`Masz na imię ${imie}`);
     }
     else{
         alert('Czemu wybrałeś anuluj?')
     }
 }
 
-// examlpe1();
-// example2();
-// example3();
